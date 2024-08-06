04fb2181efda75037f31ef227c339c6e563fd4ad
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-08-07 02:02:34 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Product from "./Product";
+
+export default function Product() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
