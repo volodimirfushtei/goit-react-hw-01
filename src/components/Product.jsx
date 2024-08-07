8ec35a819ab7e1f181d6eca4aec687474e1bd113
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-08-08 00:39:29 +0300

    add

---
+++ src/components/Product.jsx
@@ -0,0 +1,19 @@
+// src/Product.jsx
+
+// src/components/Product.jsx
+
+// src/components/Product.jsx
+
+// src/components/Product.jsx
+
+// src/components/Product.jsx
+
+export default function Product({ name, imgUrl, price }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>{" "}
+    </div>
+  );
+}
