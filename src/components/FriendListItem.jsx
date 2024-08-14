3790fe4a6fae7b181894405a65b9b0064f9ad5e6
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-08-14 13:06:24 +0300

    add t2

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,17 +1,19 @@
 import styles from "./Profile.module.css";
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
+  const statusClass = isOnline ? styles.online : styles.offline;
   return (
     <div className={styles.friend}>
       <img
         src={avatar}
         alt={`${name}'s avatar`}
         width="48"
-        className="styles.friend-avatar"
+        className="styles.avatarFre"
       />
-      <p className={styles.friend - name}>{name}</p>
-      <p className={styles.friendStatus}>{isOnline ? "Online" : "Offline"}</p>
+      <p className={styles.nameFre}>{name}</p>
+      <p className={statusClass}>{isOnline ? "Online" : "Offline"}</p>
     </div>
   );
 };
+
 export default FriendListItem;
