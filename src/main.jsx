225a07e4d0578447c7e320496107b699e8ccd121
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-08-31 00:13:30 +0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "../index.css";
 import App from "./App.jsx";
-// Render the App component in the #root div in the DOM
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(<App />);
